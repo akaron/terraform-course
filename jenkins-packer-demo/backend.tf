186b7980287cf7fa8e2c63695b98431a1a9bceb8
KaiLung Sun <akaronsun@gmail.com>
2020-08-12 09:04:28 +0800

    fix again

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-qh6dp024"
-#     key    = "terraform.tfstate"
-#     region = "us-east-2"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-xx70dpnh"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
